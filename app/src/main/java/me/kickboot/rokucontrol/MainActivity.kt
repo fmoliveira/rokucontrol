f77ef406227df3c62e32fa5c185d26ae101ed437
Filipe Oliveira <hi@fmoliveira.dev>
2022-09-25 12:56:06 -0400

    Attempt to discover Roku devices

--- app/src/main/java/me/kickboot/rokucontrol/MainActivity.kt
+++ app/src/main/java/me/kickboot/rokucontrol/MainActivity.kt
@@ -2,7 +2,13 @@ package me.kickboot.rokucontrol
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.StrictMode
+import android.util.Log
 import me.kickboot.rokucontrol.databinding.ActivityMainBinding
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.net.SocketTimeoutException
 
 class MainActivity : Activity() {
 
@@ -14,5 +20,54 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var thread = Thread(Runnable {
+            run() {
+                this.sendRokuMulticast()
+            }
+        })
+        thread.start()
+    }
+
+    private fun sendRokuMulticast(): List<String> {
+        val gatewayList = mutableListOf<String>()
+
+        val mSearch = "M-SEARCH * HTTP/1.1\n" +
+                "Host: 239.255.255.250:1900\n" +
+                "Man: \"ssdp:discover\"\n" +
+                "ST: roku:ecp"
+        val sendData = mSearch.toByteArray()
+        val sendPacket = DatagramPacket(sendData, sendData.size, InetAddress.getByName("239.255.255.250"), 1900)
+        var socket: DatagramSocket
+
+        try {
+            socket = DatagramSocket()
+        } catch (error: Exception) {
+            Log.wtf("ROKU", "Failed to open socket")
+            return gatewayList
+        }
+
+        try {
+            socket.send(sendPacket)
+        } catch (error: Exception) {
+            Log.wtf("ROKU", "Failed to send multicast")
+            return gatewayList
+        }
+
+        val receivedData = ByteArray(2048)
+        val receivePacket = DatagramPacket(receivedData, receivedData.size)
+        try {
+            socket.receive(receivePacket)
+            receivePacket.address?.hostName?.let {
+                gatewayList.add(it)
+                Log.i("ROKU", "Address received: " + it)
+            }
+        } catch (socketTimeout: SocketTimeoutException) {
+            Log.wtf("ROKU", "Timeout waiting for response")
+        } catch (error: Exception) {
+            Log.wtf("ROKU", "Failed to receive response")
+        } finally {
+            socket.close()
+        }
+        return gatewayList
     }
 }
