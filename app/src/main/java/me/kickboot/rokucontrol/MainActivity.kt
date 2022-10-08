1bce026c341bfb90374699d8e30fd1ebbd21ed84
Filipe Oliveira <hi@fmoliveira.dev>
2022-10-08 13:38:28 -0400

    Add commands to play/power/switch show

--- app/src/main/java/me/kickboot/rokucontrol/MainActivity.kt
+++ app/src/main/java/me/kickboot/rokucontrol/MainActivity.kt
@@ -2,9 +2,13 @@ package me.kickboot.rokucontrol
 
 import android.app.Activity
 import android.os.Bundle
-import android.os.StrictMode
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import me.kickboot.rokucontrol.databinding.ActivityMainBinding
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
@@ -14,20 +18,62 @@ class MainActivity : Activity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    // static channel ids from roku
+    private val channelIdNetflix = "12"
+
+    // TODO: make this list dynamic
+    private val showIdBadobi = "81154166"
+    private val showIdPokemon = "81292753"
+    private val showIdPawPatrol = "81277454"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var thread = Thread(Runnable {
-            run() {
-                this.sendRokuMulticast()
-            }
-        })
-        thread.start()
+        this.wireButtonToRokuCommand(R.id.btnPlayPause, "keypress/Play")
+        this.wireButtonToRokuCommand(R.id.btnPower, "keypress/Power")
+        this.wireButtonToRokuCommand(R.id.btnLaunchBadobi, this.getNetflixShow(showIdBadobi))
+        this.wireButtonToRokuCommand(R.id.btnLaunchPokemon, this.getNetflixShow(showIdPokemon))
+        this.wireButtonToRokuCommand(R.id.btnLaunchPawPatrol, this.getNetflixShow(showIdPawPatrol))
     }
 
+    private fun getNetflixShow(showId: String): String {
+        return "launch/$channelIdNetflix?contentID=$showId&mediaType=season"
+    }
+
+    private fun wireButtonToRokuCommand(viewId: Int, rokuCommand: String) {
+        findViewById<Button>(viewId).setOnClickListener(this.handleSendCommandToRoku(rokuCommand))
+    }
+
+    private fun handleSendCommandToRoku(command: String): View.OnClickListener {
+        return View.OnClickListener {
+            val thread = Thread(Runnable {
+                run() {
+                    this.postCommandToRoku(command)
+                }
+            })
+            thread.start()
+        }
+    }
+
+    private fun postCommandToRoku(command: String) {
+        val url = "http://192.168.2.18:8060/$command" // TODO: make the target IP dynamic
+        val empty = RequestBody.create(null, "")
+        val request = Request.Builder().url(url).method("POST", empty).build()
+        val client =  OkHttpClient()
+        try {
+            val response = client.newCall(request).execute()
+            if (response.code != 200) {
+                Log.wtf("HTTP", "Request to Roku API returned status $response.code")
+            }
+        } catch (error: Exception) {
+            Log.wtf("HTTP", "Failed to send request to Roku API", error)
+        }
+    }
+
+    // TODO: fix UPNP discovery
     private fun sendRokuMulticast(): List<String> {
         val gatewayList = mutableListOf<String>()
 
